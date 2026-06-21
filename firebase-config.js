a3dac89413bb9200fcd5ec20330bea8a2b6d63c9
Maxcuadros <max924267559@gmail.com>
2026-06-21 00:29:14 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -9,16 +9,3 @@ export const firebaseConfig = {
   measurementId: "G-5TVS3SZHV6",
 };
 
-// Debe corresponder al usuario administrador de Firebase Authentication.
-export const ADMIN_EMAIL = "mmarchena108@gmail.com";
-export const ADMIN_UID = "iAyjeSpsAITDPjVglyQgIh2IWVr2";
-export const AUTH_USERS = [
-  {
-    email: "mmarchena108@gmail.com",
-    uid: "iAyjeSpsAITDPjVglyQgIh2IWVr2"
-  },
-  {
-    email: "max924267559@gmail.com",
-    uid: "rrWXbaM2W0h8UVXYdWdnmHPqMZw1"
-  }
-]

c01db421be35b46728f823d465ed12ef7a24ee24
Maxcuadros <max924267559@gmail.com>
2026-06-21 10:57:45 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,3 @@
-// Reemplaza estos valores con los datos de Configuracion del SDK web en Firebase Console.
 export const firebaseConfig = {
   apiKey: "AIzaSyDXHDFDvvzPvBbfw2l6kdT8lhSaptbIGkc",
   authDomain: "pericotitosbellos.firebaseapp.com",
@@ -8,4 +7,16 @@ export const firebaseConfig = {
   appId: "1:1046536329612:web:996a65c5d5d51d931f5ea9",
   measurementId: "G-5TVS3SZHV6",
 };
-
+// Debe corresponder al usuario administrador de Firebase Authentication.
+export const ADMIN_EMAIL = "mmarchena108@gmail.com";
+export const ADMIN_UID = "iAyjeSpsAITDPjVglyQgIh2IWVr2";
+export const AUTH_USERS = [
+  {
+    email: "mmarchena108@gmail.com",
+    uid: "iAyjeSpsAITDPjVglyQgIh2IWVr2"
+  },
+  {
+    email: "max924267559@gmail.com",
+    uid: "rrWXbaM2W0h8UVXYdWdnmHPqMZw1"
+  }
+]
